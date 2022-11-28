1cf38e688759bee757f738fef1a4dc32c720600b
MauroDavid <mauroalosactormza@gmail.com>
2022-11-28 15:14:43 -0300

    formulario Creacion Jugador

--- client/src/components/player/CreatePlayer.jsx
+++ client/src/components/player/CreatePlayer.jsx
@@ -10,23 +10,151 @@ const CreatePlayer = () => {
 
     let dispatch = useDispatch()
 
-    React.useEffect(()=> {
+    React.useEffect(() => {
         dispatch(actions.getAllPlayers())
-    })
+        console.log(img)
+    },[dispatch])
 
-    let allPlayers = useSelector(state => state.players)  
+    let allPlayers = useSelector(state => state.players)
 
     const [newPlayer, setNewPlayer] = React.useState({
-        name:"",
-        image:"",
-        age:"",
+        name: "",
+        image: "",
+        age: "",
         birthday: "",
         description: ""
     })
 
 
+    const [errors, setErrors] = React.useState({})
+
+    const [img, setImg] = React.useState({})
 
 
-} 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const handleimg = async (e) => {
+        const files = e.target.files;
+        const data = new FormData();
+        let size = 0;
+        if (files) {
+            size += files[0].size;
+        }
+
+        data.append('file', files[0]);
+        data.append('upload_preset', 'LigaImpro');
+        setImg({
+            ...img,
+            loading:2
+        });
+        try {
+            const res = await fetch(
+                'https://api.cloudinary.com/v1_1/maurodavid/image/upload',
+                {
+                    method: 'POST',
+                    body: data
+                }
+            );
+            const file = await res.json();
+            let array = file.secure_url.split('.');
+            let format = array[array.length - 1];
+
+            if (size > 2000000) {
+                setErrors({
+                    ...errors,
+                    img: 'El archivo es demasiado grande'});
+            } else {
+                if (format === 'jpg' || format === 'png') {
+                    console.log('url de la imagen '+ file.secure_url)
+                    setErrors({
+                        errors,
+                        img:""
+                    });
+                    setImg({
+                        ...img,
+                        image: file.secure_url});
+                    setImg({
+                        ...img,
+                        loading:0
+                    });
+                    console.log('Propiedad de img '+img.image)
+                    setNewPlayer({ ...newPlayer, image: file.secure_url })
+                } else {
+                    setErrors({
+                        ...errors,
+                        img:'Solo se admiten archivos formato jpeg o png'});
+                    setImg({
+                        ...img,
+                        loading:1
+                    });
+                }
+            }
+        } catch (error) {
+            setErrors({
+                ...errors,
+                img:'Solo se admiten archivos formato jpeg o png'});
+            setImg({
+                ...img,
+                loading: 1
+            });
+        }
+    };
+
+
+    return (
+        <div>
+
+            <h1>Introducir Jugador/a</h1>
+            <form>
+                <label>Nombre del Jugador</label>
+                <br />
+                <input type="text" />
+                <br />
+                <label>Fotografía</label>
+                <br />
+                <input
+                    id="inputFile"
+                    type="file"
+                    name="image"
+                    onChange={(e) => handleimg(e)}
+                />
+                {img.loading === 2 ? (
+                    <p>
+                        Cargando imagen...
+                    </p>
+                ) : (
+                    false
+                )}
+                {img.loading === 0 ? (
+                    <div>
+                        <br />
+                        <img src={img.image} alt="" />
+                        <br />
+                    </div>
+                ) : (
+                    false
+                )}
+                {errors.img ? errors.img : false}
+                <br />
+                <label>Edad</label>
+                <br />
+                <input type="text" />
+                <br />
+                <label>Cumpleaños</label>
+                <br />
+                <input type="date" />
+                <br />
+                <label>Descripción</label>
+                <br />
+                <textarea />
+
+
+            </form>
+        </div>
+    )
+
+}
 
 export default CreatePlayer
--- client/src/components/player/Players.jsx
+++ client/src/components/player/Players.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PlayerCard from "./PlayerCard";
 import * as actions from "../../redux/actions"
+import CreatePlayer from "./CreatePlayer";
 
 
 const Players = () => {
@@ -15,10 +16,17 @@ const Players = () => {
     let players = useSelector(state => state.players)
     let admin = useSelector(state => state.admin)
 
+    const [create, setCreate] = React.useState(false)
+
+    const handleCreate = () => {
+        create?  setCreate(false) : setCreate(true)
+    }
+
     return (
         <div>
             <h1>JUGADORES</h1>
-            {admin ? <button>Agregar jugador</button> : false}
+            {admin ? <button title={create ? "cerrar panel" : "Agregar Jugador/a"} onClick={handleCreate}>{create ? "-" : "+"}</button> : false}
+            {create ? <CreatePlayer/>:false}
             {players?players.map(e => <PlayerCard name={e.name} image={e.image} id={e.id}/>):false}
         </div>
     )
